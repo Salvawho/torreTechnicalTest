e2ca0f4a034ecdaf2158734f136f254cf0b116a2
Salvador <waspwho@gmail.com>
2021-02-08 13:34:46 -0600

    adding vue + fetch

--- app/js/app.js
+++ app/js/app.js
@@ -6,39 +6,49 @@ const app = new Vue({
     },
     methods: {
         clickSearch() {
-            this.searchClicked = true
-            this.getData(this.city)
-        },
-        getData(city) {
-            let uri = ''
-            this.results = []
-            if(city == "jsdelarosa") {
-                uri = "https://torre.bio/api/bios/jsdelarosa"
-            }
-            else {  
-                alert("Please choose an option")
-            }
-            let res = []
-            if(uri != '') {
-                fetch(uri, {
-                    'mode': 'cors',
-                    "method": "GET"
-                })
-                .then(response => {
-                    if(response.ok) {
-                        response.json()
-                        .then(response1 => {
-                            this.results = response1.content.properties 
-                            if(this.results.length == 0) {
-                                alert('No listings found.')
-                            }
-                        })
-                    }
-                })
-                .catch(err => {
-                    alert("The API is facing issues. Please try again later.n" + err)
-                })          
-            }
+            var requestOptions = {
+                method: 'GET',
+                redirect: 'follow'
+              };
+              
+              fetch("https://torre.bio/api/bios/jsdelarosa", requestOptions)
+                .then(response => response.text())
+                .then(result => console.log(result))
+                .catch(error => console.log('error', error));
         }
+        //     this.searchClicked = true
+        //     this.getData(this.city)
+        // },
+        // getData(city) {
+        //     let uri = ''
+        //     this.results = []
+        //     if(city == "jsdelarosa") {
+        //         uri = "https://torre.bio/api/bios/jsdelarosa"
+        //     }
+        //     else {  
+        //         alert("Please choose an option")
+        //     }
+        //     let res = []
+        //     if(uri != '') {
+        //         fetch(uri, {
+        //             'mode': 'cors',
+        //             "method": "GET"
+        //         })
+        //         .then(response => {
+        //             if(response.ok) {
+        //                 response.json()
+        //                 .then(response1 => {
+        //                     this.results = response1.content.properties 
+        //                     if(this.results.length == 0) {
+        //                         alert('No listings found.')
+        //                     }
+        //                 })
+        //             }
+        //         })
+        //         .catch(err => {
+        //             alert("The API is facing issues. Please try again later.n" + err)
+        //         })          
+        //     }
+        // }
     }
 })
